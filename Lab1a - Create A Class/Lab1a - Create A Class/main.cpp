83188abb1cb2f1c5b2b4e5a211cbabd0da25e08e
JamesWilson <james.t.wilson3.ctr@navy.mil>
2017-02-14 15:08:31 -0800

    Fixed broken code

--- Lab1a - Create A Class/Lab1a - Create A Class/main.cpp
+++ Lab1a - Create A Class/Lab1a - Create A Class/main.cpp
@@ -4,24 +4,9 @@
 
 using namespace std;
 
-string GetFinnGreeting()
-{
-	return "Hi, my name is Finn and I'm 14!";
-}
-
-string GetJakeGreeting()
-{
-	return "Hi, my name is Jake and I'm 28!";
-}
-
-
-
 int main() {
 	auto finn = Character("Finn", 14);
-	auto jake = Character("Jake", 28);	
-
-	finn.age = 10194823;
-	finn.name = "Poopy Poop";
+	auto jake = Character("Jake", 28);
 
 	cout
 		<< finn.Greet() << endl
